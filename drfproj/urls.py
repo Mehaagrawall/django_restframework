da79e719e4848e9f7816c35df94fcd8563e5576c
Mehaagrawall <mehaagrawalsdl@gmail.com>
2024-06-29 00:23:53 +0530

    Update urls.py

--- drfproj/urls.py
+++ drfproj/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from .views import TesView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', )
+    path('api_auth/',include('rest_framework.urls')),
+    path('', TesView.as_view(), name='test')
 ]
