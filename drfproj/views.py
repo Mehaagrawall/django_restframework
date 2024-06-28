977524c3cc8e45083590c4df6dc8f545ea8d254a
Mehaagrawall <mehaagrawalsdl@gmail.com>
2024-06-29 00:14:37 +0530

    Update views.py

--- drfproj/views.py
+++ drfproj/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-dfvdf
+class TesView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'username' : 'admin',
+            'years active': 10
+        }
+        return Response(data)
